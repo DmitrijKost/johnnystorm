97d4bf55ca6bb35eb54deb29f845840f3da7a89f
oleggorru <oleggorru@gmail.ru>
2014-12-01 12:26:31 +0300

    0.3

--- gitcoverage/repoConverter.py
+++ gitcoverage/repoConverter.py
@@ -12,13 +12,19 @@ pathToAllRepositories="D:/develop/python/cod/testgit/"
 
 TestRepo=Repo.clone(Repo("D:/develop/python/cod/testgit/clonefnts"),"D:/develop/python/cod/testgit/clonefnts2")
 
+def testExecutor():
+    repo,modRep,dir =dowloadRep("https://bitbucket.org/thegoodguysteam/mister-fantastic/get/435fceb5611f.zip","mistrer fantastic")
+    obrRepByCommit(dir,[repo.heads.master.commit,],repo=repo)
+
+
 #метод получения всех файлов в репозитории определенного коммита в входных данных полный путь до репозитория
 def dowloadRep(url,nameRep,dirReps=pathToAllRepositories):
     if(nameRep in os.listdir(dirReps)):
         nameRep=createNewName(dirReps,nameRep)
     repo = Repo.clone_from(url,dirReps+nameRep)
+    #бд
     modRep=requestsDzdb.putRep(nameRep,url)
-    return repo
+    return repo,modRep,nameRep
 
 #Вспомагательный метод для dowloadRepAndUse запускается, когда в проекте уже лежит репозиторий с таким именем
 #Предположительно будет использоваться при многопоточном обработке репозиториев или если возникла
@@ -33,29 +39,32 @@ def createNewName(dir,name):
     return name
 
 #Данный метод будет запускать обработку репозитория по переданным ему именам(хеш) коммитов:
-#TODO атрибут repo
-def obrRepByCommit(dir,listCommits,repo=TestRepo):
+def obrRepByCommit(dir,listBranch,listCommits,repo=TestRepo):
     #Какая-нибудь валидация listCommits
+    #for branch in listBranch:
+    #    requestsDzdb.putBranch(repo=repo,name=branch)
     for commit in listCommits:
-        #TODO добавить commit в бд
+        #бд
+        requestsDzdb.putCommit(commit=str(commit),nameBrench='head')
         dirCom=makeDirCommit(dir,commit)
         #TODO метод который будет воссоздавать репозиторий на момент когда в репозитории данный коммит являлся хедом
         #TODO пока вместо верхнего делаем так. затем метод убрать, когда верхний будет готов
         Repo.clone(repo,dirCom)
 
-        walk(dirCom,commit)
+        walk(dirCom,commit,repo)
 
     #TODO все ли коммиты из листа были обработаны и соотвествующие действия при отрицательном ответе
 
 #Создание рабочей области(папки) для обработки определенного коммита
 def makeDirCommit(dir,commit):
+    #E
     dirCom=dir+"/workplace/"+commit
     os.makedirs(dirCom)
     return dirCom
 
 #метод который обходит репозиторий, который находится в том состояние в котором он был,
 #при коммите переданным ему в аргументах.
-def walk(dir,commit="Unknown"):
+def walk(dir,commit="Unknown",repo=None):
     if(commit=="Unknown"):
         return "Exception: Unknown commit in atribytes"
     #TODO проверка на наличие коммита в бд
@@ -63,8 +72,18 @@ def walk(dir,commit="Unknown"):
         path = os.path.join(dir, name)
         if os.path.isfile(path):
             print path
-            #TODO cложить файл в бд
-            #TODO запустить метод обработки данного файла
+            # cложить файл в бд
+            blob = requestsDzdb.putBlob(path,commit=commit)
+            # запустить метод обработки данного файла
+            g=repo.git
+            c = g.execute("git blame -t  wsgi\\openshift\\openshiftlibs.py ")
+            c=c.split('\n')
+            for i in c:
+                k = c[i].split(' ')
+                name = k[1][1:]
+                nom=k[5][:1]
+                requestsDzdb.putLine(nom,name,path,blob)
+
         else:
             walk(path)
 
--- gitcoverage/requestsDzdb.py
+++ gitcoverage/requestsDzdb.py
@@ -1,3 +1,4 @@
+# coding: utf8
 __author__ = 'oleg'
 
 #TODO метод добавления репозитория,комита,блоба,строки в репозиторий
@@ -8,27 +9,41 @@ def putRep(URL):
     repo.save()
     return repo
 
-def putBranch(name,URL):
-    repo=Repository.objects.get(url=URL)
-    branch=Branch(id_repo=repo,name=name)
-    branch.save()
-    return branch
+def putBranch(name,URL,repo=None):
+    if(repo==None):
+        repo=Repository.objects.get(url=URL)
+    if(repo!=None&name!=None):
+        branch=Branch(id_repo=repo,name=name)
+        branch.save()
+        return branch
+    return None
 
-def putCommit(ssh,nameBrench):
-    branch=Branch.objects.get(name=nameBrench)
-    commit=Commit(id_branch=branch,ssh=ssh)
-    commit.save()
-    return commit
+def putCommit(ssh,nameBrench,branch=None):
+    if(branch==None):
+        #branch=Branch.objects.get(name=nameBrench)
+        branch='head'
+    if(ssh!=None&branch!=None):
+        commit=Commit(id_branch=branch,ssh=ssh)
+        commit.save()
+        return commit
+    return None
 
-def putBlob(path,sshCom):
-    com=Commit.objects.get(ssh=sshCom)
-    blob=Blob(id_commit=com,path_to_file=path)
-    blob.save()
-    return blob
+def putBlob(path,sshCom,commit=None):
+    if(commit==None):
+        commit=Commit.objects.get(ssh=sshCom)
+    if(commit!=None&path!=None):
+        blob=Blob(id_commit=commit,path_to_file=path)
+        blob.save()
+        return blob
+    return None
 
-def putLine(pathBlob,sshCom,number,autor):
-    com=Commit.objects.get(ssh=sshCom)
-    blob=Blob.objects.get(id=com,path_to_file=pathBlob)
+def putLine(number,autor,pathBlob,sshCom,blob=None):
+    if(blob==None):
+        com=Commit.objects.get(ssh=sshCom)
+        blob=Blob.objects.get(id=com,path_to_file=pathBlob)
+    if(blob!=None&number!=None):
+        if(autor==None):
+            autor="Unknown programmer"
     line=Line(id_blob=blob,number=number,owner=autor)
     line.save()
     return putLine
